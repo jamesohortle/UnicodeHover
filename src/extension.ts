2d36b7ec3d275f11da65a3b1271518051e5c181c
jamesohortle <jamesohortle@gmail.com>
2019-11-07 11:11:29 +0900

    Provide hover for TeX files: \char, \Uchar and primitive (caret ^) forms (oct, dec & hex).

--- src/extension.ts
+++ src/extension.ts
@@ -115,12 +115,86 @@ class JSUnicodeHover implements vscode.HoverProvider {
 
 	public dispose() { }
 }
+
+class TexUnicodeHover implements vscode.HoverProvider {
+	public provideHover(
+		document: vscode.TextDocument, position: vscode.Position, _token: vscode.CancellationToken
+	): Thenable<vscode.Hover> {
+		let unicodeRegexAny = new RegExp(/(\\U?char['"]?[\da-fA-F]{1,8}|\^{2,6}[\da-f]{2,6})/); // General form.
+		let unicodeRegexCharDec = new RegExp(/\\char([\d]{1,7})/); // Decimal form: \char98
+		let unicodeRegexCharOct = new RegExp(/\\char'([\da-fA-F]{1,8})/); // Octal form: \char'98.
+		let unicodeRegexCharHex = new RegExp(/\\char"([\da-fA-F]{1,8})/); // Hexadecimal form: \char"98.
+		let unicodeRegexUcharDec = new RegExp(/\\Uchar([\d]{1,7})/); // Decimal form: \Uchar98
+		let unicodeRegexUcharOct = new RegExp(/\\Uchar'([0-7]{1,8})/); // Octal form: \Uchar'98.
+		let unicodeRegexUcharHex = new RegExp(/\\Uchar"([\da-fA-F]{1,8})/); // Hexadecimal form: \Uchar"98.
+		let unicodeRegexPrimitive6 = new RegExp(/\^{6}([\da-f]{6})/); // Hexadecimal form: ^^^^^^0000ff
+		let unicodeRegexPrimitive4 = new RegExp(/\^{4}([\da-f]{4})/); // 4 hexadecimal: ^^^^00ff
+		let unicodeRegexPrimitive2 = new RegExp(/\^{2}([\da-f]{2})/); // 2 hexadecimal: ^^ff.
+
+		const range = document.getWordRangeAtPosition(position, unicodeRegexAny);
+
+		if (range === undefined) {
+			return new Promise((_resolve, reject) => {
+				reject("Range undefined.");
+			});
+		}
+		const word = document.getText(range);
+		console.log(`TX ${word}`);
+		return new Promise((resolve, reject) => {
+			if (word.match(unicodeRegexCharDec)) {
+				let codePoint = parseInt(word.match(unicodeRegexCharDec)![1], 10);
+				resolve(new vscode.Hover(String.fromCharCode(codePoint)));
+				return;
+			} else if (word.match(unicodeRegexCharOct)) {
+				let codePoint = parseInt(word.match(unicodeRegexCharOct)![1], 8);
+				resolve(new vscode.Hover(String.fromCharCode(codePoint)));
+				return;
+			} else if (word.match(unicodeRegexCharHex)) {
+				let codePoint = parseInt(word.match(unicodeRegexCharHex)![1], 16);
+				resolve(new vscode.Hover(String.fromCharCode(codePoint)));
+				return;
+			} else if (word.match(unicodeRegexUcharDec)) {
+				let codePoint = parseInt(word.match(unicodeRegexUcharDec)![1], 10);
+				resolve(new vscode.Hover(String.fromCharCode(codePoint)));
+				return;
+			}
+			else if (word.match(unicodeRegexUcharOct)) {
+				let codePoint = parseInt(word.match(unicodeRegexUcharOct)![1], 8);
+				resolve(new vscode.Hover(String.fromCharCode(codePoint)));
+				return;
+			} else if (word.match(unicodeRegexUcharHex)) {
+				let codePoint = parseInt(word.match(unicodeRegexUcharHex)![1], 16);
+				resolve(new vscode.Hover(String.fromCharCode(codePoint)));
+				return;
+			} else if (word.match(unicodeRegexPrimitive6)) {
+				let codePoint = parseInt(word.match(unicodeRegexPrimitive6)![1], 16);
+				resolve(new vscode.Hover(String.fromCharCode(codePoint)));
+				return;
+			}
+			else if (word.match(unicodeRegexPrimitive4)) {
+				let codePoint = parseInt(word.match(unicodeRegexPrimitive4)![1], 16);
+				resolve(new vscode.Hover(String.fromCharCode(codePoint)));
+				return;
+			} else if (word.match(unicodeRegexPrimitive2)) {
+				let codePoint = parseInt(word.match(unicodeRegexPrimitive2)![1], 16);
+				resolve(new vscode.Hover(String.fromCharCode(codePoint)));
+				return;
+			} else {
+				reject("Not a Unicode escape.");
+			}
+		});
+	}
+
+	public dispose() { }
+}
+
 // Provide the hovers.
 export function activate(context: vscode.ExtensionContext): void {
 	context.subscriptions.push(vscode.languages.registerHoverProvider({ scheme: 'file' }, new UnicodeHover()));
 	context.subscriptions.push(vscode.languages.registerHoverProvider({ scheme: 'file', language: 'python' }, new PyUnicodeHover()));
 	context.subscriptions.push(vscode.languages.registerHoverProvider({ scheme: 'file', language: 'javascript' }, new JSUnicodeHover()));
-	console.log("UnicodeHover: provider pushed.");
+	context.subscriptions.push(vscode.languages.registerHoverProvider({ scheme: 'file', language: 'latex' }, new TexUnicodeHover()));
+	console.log("UnicodeHover: providers pushed.");
 }
 
 // this method is called when your extension is deactivated
