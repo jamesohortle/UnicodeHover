961423aadd781b11bdb2fbb1ff6adfd8a13d0531
jamesohortle <jamesohortle@gmail.com>
2019-11-07 11:04:56 +0900

    Provide hover for Python files: \u1234, \U1234abcd, etc.

--- src/extension.ts
+++ src/extension.ts
@@ -33,9 +33,46 @@ class UnicodeHover implements vscode.HoverProvider {
 	public dispose() { }
 }
 
+/**
+ * Provide a hover for Python files and the Unicode escapes therein.
+ * Python has two simple forms: \u1234 (small u and 4 hexadecimal digits)
+ * or \U1234abcd (big U and 8 hexadecimal digits).
+ */
+class PyUnicodeHover implements vscode.HoverProvider {
+	public provideHover(
+		document: vscode.TextDocument, position: vscode.Position, _token: vscode.CancellationToken
+	): Thenable<vscode.Hover> {
+		let unicodeRegexAny = new RegExp(/\\(u|U)([\da-fA-F]{4,8})/); // General form.
+		let unicodeRegex4 = new RegExp(/\\u([\da-fA-F]{4})/); // Short form: "\uabcd".
+		let unicodeRegex8 = new RegExp(/\\U([\da-fA-F]{8})/); // Long form: "\Uabcd1234".
+
+		const range = document.getWordRangeAtPosition(position, unicodeRegexAny);
+		if (range === undefined) {
+			return new Promise((_resolve, reject) => {
+				reject("Range undefined.");
+			});
+		}
+		const word = document.getText(range);
+		console.log(`PH ${word}`);
+		return new Promise((resolve, reject) => {
+			if (word.match(unicodeRegex8)) {
+				let codePoint = parseInt(word.match(unicodeRegex8)![1], 16);
+				resolve(new vscode.Hover(String.fromCharCode(codePoint)));
+			} else if (word.match(unicodeRegex4)) {
+				let codePoint = parseInt(word.match(unicodeRegex4)![1], 16);
+				resolve(new vscode.Hover(String.fromCharCode(codePoint)));
+			} else {
+				reject("Not a Unicode escape.");
+			}
+		});
+	}
+
+	public dispose() { }
+}
 // Provide the hovers.
 export function activate(context: vscode.ExtensionContext): void {
 	context.subscriptions.push(vscode.languages.registerHoverProvider({ scheme: 'file' }, new UnicodeHover()));
+	context.subscriptions.push(vscode.languages.registerHoverProvider({ scheme: 'file', language: 'python' }, new PyUnicodeHover()));
 	console.log("UnicodeHover: provider pushed.");
 }
 
