eb7612a150488d35cdc2cadabcb7f09b25dd0f7d
jamesohortle <jamesohortle@gmail.com>
2019-11-13 11:37:11 +0900

    Process Unicode data with Python (3.8).

---
+++ scripts/process_unicode_data.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*
+"""Process the Unicode Data for the extension."""
+from __future__ import annotations
+from typing import *
+
+import re
+import json
+import sqlite3
+from pathlib import Path
+from itertools import chain
+from collections import OrderedDict as OrdDict
+
+import lxml.builder as builder
+from lxml import etree as ET
+
+# Check if UCD exists.
+HERE = Path(__file__).parent
+DATA = (HERE / ".." / "data").resolve()
+NO_UNIHAN_XML = DATA / "ucd.nounihan.flat.xml"
+UNIHAN_XML = DATA / "ucd.unihan.flat.xml"
+
+if not NO_UNIHAN_XML.is_file():
+    quit(
+        "Run `wget https://www.unicode.org/Public/UCD/latest/ucdxml/ucd.nounihan.flat.zip && unzip ucd.nounihan.flat.zip` to get the XML file. (File sizes: .zip=912K; .xml: 51M.)"
+    )
+elif not UNIHAN_XML.is_file():
+    quit(
+        "Run `wget https://www.unicode.org/Public/UCD/latest/ucdxml/ucd.unihan.flat.zip && unzip ucd.unihan.flat.zip` to get the XML file. (File sizes: .zip=6.1M; .xml: 40M.)"
+    )
+
+# Define tags for the XML.
+elt = builder.ElementMaker(namespace="http://www.unicode.org/ns/2003/ucd/1.0")
+char_tag = elt.char().tag
+
+# # Define Unihan ranges.
+# UNIHAN_LO = 0x4E00
+# UNIHAN_HI = 0x9FFF
+# UNIHAN_EXT_A_LO = 0x3400
+# UNIHAN_EXT_A_HI = 0x4DBF
+# UNIHAN_EXT_B_LO = 0x20000
+# UNIHAN_EXT_B_HI = 0x2A6DF
+# UNIHAN_EXT_C_LO = 0x2A700
+# UNIHAN_EXT_C_HI = 0x2B73F
+# UNIHAN_EXT_D_LO = 0x2B740
+# UNIHAN_EXT_D_HI = 0x2B81F
+
+
+def get_alias(char_elt: ET.Element) -> str:
+    aliases: List[str] = []
+    for tag in char_elt.getchildren():
+        aliases.append(tag.get("alias"))
+    aliases = sorted(aliases, key=lambda a: len(a), reverse=True)
+    return aliases[0] + " (alias)"
+
+
+ideograph_octothorpe = re.compile(r"-#$")
+
+
+def yield_non_unihan(
+    path: Path = NO_UNIHAN_XML
+) -> Generator[Tuple[int, str], None, None]:
+    """Parse non-Unihan data."""
+    _path = str(path.resolve())
+    chars = ET.iterparse(_path, tag=char_tag, recover=True, huge_tree=True)
+    for _, char in chars:
+        codepoint = char.get("cp")
+        if not codepoint:
+            continue
+        block = char.get("blk")
+        if block == "Nushu":
+            _name = ideograph_octothorpe.sub(" ", char.get("na"))
+            nushu_duben = "(女書讀本: " + (char.get("kSrc_NushuDuben") or "?") + ")"
+            name = _name + nushu_duben
+        elif block == "Tangut":
+            _name = ideograph_octothorpe.sub(" ", char.get("na"))
+            tangut_source = "(Ref: " + (char.get("kTGT_MergedSrc") or "?") + ")"
+            name = _name + tangut_source
+        else:
+            name = char.get("na") or char.get("na1") or get_alias(char)
+        yield (int(codepoint, base=16), name.title())
+
+
+def yield_unihan(path: Path = UNIHAN_XML) -> Generator[Tuple[int, str], None, None]:
+    """Parse Unihan data."""
+    _path = str(path.resolve())
+    unihan = ET.iterparse(_path, tag=char_tag, recover=True, huge_tree=True)
+    for _, char in unihan:
+        codepoint = char.get("cp")
+        name = (char.get("kDefinition") or "(No description)") + " (Unified CJK)"
+        name = name[0].upper() + name[1:]
+        yield (int(codepoint, base=16), name)
+
+
+# Define JSON outfile.
+JSON_OUT = DATA / "unicode_data.json"
+
+if __name__ == "__main__":
+    # Order the data by codepoint.
+    unicode_data = {
+        codepoint: desc for codepoint, desc in chain(yield_non_unihan(), yield_unihan())
+    }
+
+    # Dump to JSON.
+    with JSON_OUT.open(mode="w", encoding="utf-8") as json_out:
+        json.dump(unicode_data, json_out, indent="\t", sort_keys=True)
+
