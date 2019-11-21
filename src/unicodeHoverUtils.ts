6e59e13c1cb89d42f2010d7fe7657a3117f2aaaa
jamesohortle <jamesohortle@gmail.com>
2019-11-21 10:16:07 +0900

    Fix rendering (fixes #9 ).

--- src/unicodeHoverUtils.ts
+++ src/unicodeHoverUtils.ts
@@ -29,6 +29,7 @@ export function makeMarkdown(codePoint: number, isSurrogate: boolean = false): v
 	const description = getUnicodeData(codePoint);
 	const externalLink = "https://unicode-table.com/en/" + (codePoint <= MAX_CODE_POINT ? codePoint.toString(16).toUpperCase() : "");
 	const surrogateNotification = isSurrogate ? " (surrogate pair) " : "";
-	const markdown = `${glyph} [${description}](${externalLink})${surrogateNotification} (UnicodeHover)`;
+	// Add a zero width space to prevent `#` etc from affecting markdown rendering.
+	const markdown = `\u200b${glyph} [${description}](${externalLink})${surrogateNotification} (UnicodeHover)`;
 	return new vscode.MarkdownString(markdown);
 }
