e550fbe82af589993befad4af871af218929a9be
James Hortle <jamesohortle@gmail.com>
2020-09-11 00:55:25 +1000

    Support Haskell-specific escapes.

--- src/haskellHover.ts
+++ src/haskellHover.ts
@@ -1,5 +1,9 @@
 import * as vscode from "vscode";
+import * as haskellEscapesJson from "./haskellEscapes.json";
 import { makeMarkdown } from "./unicodeHoverUtils";
+
+// Parse the Haskell-specific escapes.
+const haskellEscapes = JSON.parse(JSON.stringify(haskellEscapesJson));
 
 /**
  * Provide a hover for Haskell files and the Unicode escapes therein.
@@ -7,6 +11,61 @@ import { makeMarkdown } from "./unicodeHoverUtils";
  */
 export class HaskellHover implements vscode.HoverProvider {
     public provideHover(document: vscode.TextDocument, position: vscode.Position, _token: vscode.CancellationToken): Thenable<vscode.Hover> {
+
+        /**
+         * Check for Haskell-specific escapes first.
+         * If they don't resolve, continue to Unicode regexes.
+         */
+        const singleCharEscape = new RegExp(/\\[0abfnrtv"&'\\]/);
+        const asciiAbbr = new RegExp(/\\[1234ABCDEFGHIKLMNOPQRSTUVXY]{2,3}/);
+        const charEscapes = new RegExp(/\\\^[A-Z@\\\[\]\^_]/);
+
+        const r1 = document.getWordRangeAtPosition(position, singleCharEscape);
+        if (r1 !== undefined) {
+            const word = document.getText(r1);
+            console.log(`SCES ${word}`);
+
+            const esc = haskellEscapes![word];
+            if (esc !== undefined) {
+                const markdown = makeMarkdown(esc);
+                return new Promise((resolve, _reject) => {
+                    resolve(new vscode.Hover(markdown));
+                });
+            }
+
+        }
+
+        const r2 = document.getWordRangeAtPosition(position, asciiAbbr);
+        if (r2 !== undefined) {
+            const word = document.getText(r2);
+            console.log(`AABBR ${word}`);
+
+            const esc = haskellEscapes![word];
+            if (esc !== undefined) {
+                const markdown = makeMarkdown(esc);
+                return new Promise((resolve, _reject) => {
+                    resolve(new vscode.Hover(markdown));
+                });
+            }
+        }
+
+        const r3 = document.getWordRangeAtPosition(position, charEscapes);
+        if (r3 !== undefined) {
+            const word = document.getText(r3);
+            console.log(`CESC ${document.getText(r3)}`);
+
+            const esc = haskellEscapes![word];
+            if (esc !== undefined) {
+                const markdown = makeMarkdown(esc);
+                return new Promise((resolve, _reject) => {
+                    resolve(new vscode.Hover(markdown));
+                });
+            }
+        }
+
+        /**
+         * Check Unicode regexes.
+         */
         const unicodeRegexAny = new RegExp(/\\(x|o)?[a-fA-F\d]+/);
         const unicodeRegexOct = new RegExp(/\\o([0-7]+)/);
         const unicodeRegexDec = new RegExp(/\\(\d+)/);
